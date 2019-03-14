10308e8934b2e4e52ae023cafa176703f82a528f
Onur Küçük <onur@delipenguen.net>
2019-03-14 23:16:59 +0300

    use alternative method to modify project settings tab

--- lib/redmine_default_assignee/patches/projects_helper_patch.rb
+++ lib/redmine_default_assignee/patches/projects_helper_patch.rb
@@ -1,41 +1,27 @@
 require_dependency 'projects_helper'
 
-
 module RedmineDefaultAssignee
   module Patches
     module ProjectsHelperPatch
-      def self.included(base)
-        base.extend(ClassMethods)
-        base.send(:include, InstanceMethods)
-        base.class_eval do
-          alias_method_chain :project_settings_tabs, :redmine_default_assignee
-        end
-      end
 
-      module ClassMethods
+      def project_settings_tabs
+        tabs = super
 
-      end
-
-      module InstanceMethods
-
-        def project_settings_tabs_with_redmine_default_assignee
-          tabs = project_settings_tabs_without_redmine_default_assignee
-
-          if User.current.allowed_to?(:rda_default_assignee, @project)
-            tabs << {
-              :name => 'rda_project_settings',
-              :action => :rda_manage_project_settings,
-              :partial => 'projects/rda_project_settings',
-              :label => 'redmine_default_assignee.project_settings.label_default_assignee'
-            }
-          end
-
-          tabs
+        if User.current.allowed_to?(:rda_default_assignee, @project)
+          tabs << {
+            :name => 'rda_project_settings',
+            :action => :rda_manage_project_settings,
+            :partial => 'projects/rda_project_settings',
+            :label => 'redmine_default_assignee.project_settings.label_default_assignee'
+          }
         end
 
+        tabs
       end
 
     end
   end
 end
+
+ProjectsController.helper(RedmineDefaultAssignee::Patches::ProjectsHelperPatch)
 
