615c0992121ef98c3423a2fd6c7b8b3c9c44dfeb
Onur Küçük <onur@delipenguen.net>
2021-10-12 00:24:45 +0300

    allow selecting issue author

--- lib/redmine_default_assignee.rb
+++ lib/redmine_default_assignee.rb
@@ -2,6 +2,8 @@ require 'redmine_default_assignee/hooks/views_issues_hook'
 
 module RedmineDefaultAssignee
 
+  SELECTED_USER_AUTHOR_ID = -2
+
   def self.settings
     Setting[:plugin_redmine_default_assignee]
   end
