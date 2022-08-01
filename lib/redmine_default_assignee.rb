4b14fa65916c4fec9461c4d95fbe6cdc0da02d7f
Onur Küçük <onur@delipenguen.net>
2022-08-01 23:15:49 +0300

    selecting author means issue author, selecting me means current user

--- lib/redmine_default_assignee.rb
+++ lib/redmine_default_assignee.rb
@@ -3,6 +3,7 @@ require 'redmine_default_assignee/hooks/views_issues_hook'
 module RedmineDefaultAssignee
 
   SELECTED_USER_AUTHOR_ID = -2
+  SELECTED_USER_CURRENT_USER_ID = -3
 
   def self.settings
     Setting[:plugin_redmine_default_assignee]
