a8c679a4157b2c923c706a3b01509b4605250f55
Onur Küçük <onur@delipenguen.net>
2023-02-12 02:20:51 +0300

    update assignee on project issues context menu too

--- lib/redmine_default_assignee/patches/issues_controller_patch.rb
+++ lib/redmine_default_assignee/patches/issues_controller_patch.rb
@@ -14,9 +14,34 @@ module RedmineDefaultAssignee
           rda_new_assigned_to_id = rda_settings.default_assignee[rda_changing_status_id]
 
           if rda_settings.apply_on_context_menu && rda_new_assigned_to_id.present?
-            Rails.logger.info "[DefaultAssignee] changing assigned_to_id to #{rda_new_assigned_to_id.inspect}" \
-              "as it is set for status_id #{rda_changing_status_id.inspect} in settings"
+            Rails.logger.info '[DefaultAssignee] bulk update changing assigned_to_id to ' \
+              "#{rda_new_assigned_to_id.inspect} as it is set for status_id #{rda_changing_status_id.inspect}" \
+              'in settings'
             params[:issue]['assigned_to_id'] = rda_new_assigned_to_id
+          end
+        end
+
+        super
+      end
+
+      def update
+        rda_changing_status_id = params[:issue]['status_id']
+
+        if rda_changing_status_id.present?
+          rda_back_url = Rails.application.routes.recognize_path(params[:back_url])
+
+          if rda_back_url[:controller] == 'issues' && rda_back_url[:action] == 'index'
+            rda_current_project_id = rda_back_url[:project_id]
+            rda_current_project = Project.find_by_identifier(rda_current_project_id)
+            rda_settings = RdaProjectSetting.settings_for_project(rda_current_project)
+            rda_new_assigned_to_id = rda_settings.default_assignee[rda_changing_status_id]
+
+            if rda_settings.apply_on_context_menu && rda_new_assigned_to_id.present?
+              Rails.logger.info '[DefaultAssignee] project issues update changing assigned_to_id to ' \
+                "#{rda_new_assigned_to_id.inspect} as it is set for status_id #{rda_changing_status_id.inspect}" \
+                'in settings'
+              params[:issue]['assigned_to_id'] = rda_new_assigned_to_id
+            end
           end
         end
 
